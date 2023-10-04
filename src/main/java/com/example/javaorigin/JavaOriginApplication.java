e78f932f14a8e6f94d19037bafcfd128fbd06258
clapjeans <tnwls96101@daum.net>
2023-10-04 21:57:11 +0900

    자바의 정석 테스트

---
+++ src/main/java/com/example/javaorigin/JavaOriginApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaorigin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOriginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOriginApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/javaorigin/generics/FruitBoxEx1.java
@@ -0,0 +1,6 @@
+package com.example.javaorigin.generics;
+
+public class FruitBoxEx1 {
+
+    String Hi = "Hi";
+}
